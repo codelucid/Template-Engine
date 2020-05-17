b607f14caeab7412de5044954ef26e79f1b78848
Coop <61427314+codelucid@users.noreply.github.com>
2020-05-16 20:55:14 -0400

    tests pass and can cycle through questions

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
 function createTeam() {
     console.log("Please build your team!");
     let manager = new Manager(inquirer.prompt([
@@ -20,167 +21,260 @@ function createTeam() {
         type: "input",
         message: "What is your manager's name?",
         name: "name",
-        validate: answer => {
-            if (answer !== "") {
-                return true;
-            } else
-            return "Please enter your name.";
-        }
+        // validate: function validateName(name) {
+        //     return name !== "";
+        // }
     },
     {
         type: "input",
         message: "What is your manager's ID?",
         name: "id",
-        validate: validateNumber
+        // validate: answer => {
+        //     if (answer !== "number") {
+        //         return true;
+        //     } else
+        //     return "Please enter your ID number.";
+        // }
     },
     {
         type: "input",
         message: "What is your manager's email?",
         name: "email",
-        validate: validateEmail
+        // validate: answer => {
+        //     if (answer !== "name@domain") {
+        //         return true;
+        //     } else
+        //     return "Please enter your email.";
+        // }
     },
     {
         type: "input",
         message: "What is your manager's office number?",
         name: "officeNumber",
-        validate: answer => {
-            if (answer !== "number") {
-                return true;
-            } else
-            return "Please enter your office number.";
-        }
-    }, 
-    ])) 
-    .then(inquirer.prompt([
+        // validate: answer => {
+        //     if (answer !== "number") {
+        //         return true;
+        //     } else
+        //     return "Please enter your office number.";
+        // }
+    },
     {
         type: "list",
         message: "Which type of team member would you like to add?",
         name: "newMember",
-        choices: ["Engineer", "Intern", "I don't need any more team members."]
-    }
-    ]))
+        choices: ["Engineer", "Intern", "I don't need anymore team members."],
+    },
+    ]).then(val => {
+        if (val.newMember === "Engineer") {
+            return engineerTeam();
+        } else if (val.newMember === "Intern") {
+            return internTeam();
+        } else  
+            return quit();
+        
+    })
+    ); 
+    
+};   
+
+
+function engineerTeam() {    
     let engineer = new Engineer(inquirer.prompt([
     {
         type: "input",
         message: "What is your engineer's name?",
         name: "name",
-        validate: answer => {
-            if (answer !== "") {
-                return true;
-            } else
-            return "Please enter your name.";
-        }
+        // validate: answer => {
+        //     if (answer !== "") {
+        //         return true;
+        //     } else
+        //     return "Please enter your name.";
+        // }
     },
     {
         type: "input",
         message: "What is your engineer's ID?",
         name: "id",
-        validate: answer => {
-            if (answer !== "number") {
-                return true;
-            } else
-            return "Please enter your ID number.";
-        }
+        // validate: answer => {
+        //     if (answer !== "number") {
+        //         return true;
+        //     } else
+        //     return "Please enter your ID number.";
+        // }
     },
     {
         type: "input",
         message: "What is your engineer's email?",
         name: "email",
-        validate: answer => {
-            if (answer !== "") {
-                return true;
-            } else
-            return "Please enter your email.";
-        }
+        // validate: answer => {
+        //     if (answer !== "name@domain") {
+        //         return true;
+        //     } else
+        //     return "Please enter your email.";
+        // }
     },
     {
         type: "input",
         message: "What is your engineer's GitHub name?",
         name: "gitHub",
-        validate: answer => {
-            if (answer !== "") {
-                return true;
-            } else
-            return "Please enter your GitHub name.";
-        }
+        // validate: answer => {
+        //     if (answer !== "") {
+        //         return true;
+        //     } else
+        //     return "Please enter your GitHub name.";
+        // }
     },
-    ]))
-    .then(inquirer.prompt([
     {
         type: "list",
         message: "Which type of team member would you like to add?",
         name: "newMember",
-        choices: ["Engineer", "Intern", "I don't need any more team members."]
-    }
-    ]))
+        choices: ["Engineer", "Intern", "I don't need anymore team members."],
+    },
+    ]).then(val => {
+        if (val.newMember === "Engineer") {
+            return engineerTeam();
+        } else if (val.newMember === "Intern") {
+            return internTeam();
+        } else  
+            return quit();
+        
+    })
+    );
+};
+
+
+function internTeam() {
     let intern = new Intern(inquirer.prompt([
     {
         type: "input",
         message: "What is your intern's name?",
         name: "name",
-        validate: answer => {
-            if (answer !== "") {
-                return true;
-            } else
-            return "Please enter your name.";
-        }
+        // validate: answer => {
+        //     if (answer !== "") {
+        //         return true;
+        //     } else
+        //     return "Please enter your name.";
+        // }
     },
     {
         type: "input",
         message: "What is your intern's ID?",
         name: "id",
-        validate: answer => {
-            if (answer !== "number") {
-                return true;
-            } else
-            return "Please enter your ID number.";
-        }
+        // validate: answer => {
+        //     if (answer !== "number") {
+        //         return true;
+        //     } else
+        //     return "Please enter your ID number.";
+        // }
     },
     {
         type: "input",
         message: "What is your intern's email?",
         name: "email",
-        validate: answer => {
-            if (answer !== "") {
-                return true;
-            } else
-            return "Please enter your email.";
-        }
+        // validate: answer => {
+        //     if (answer !== "") {
+        //         return true;
+        //     } else
+        //     return "Please enter your email.";
+        // }
     },
     {
         type: "input",
         message: "What is your intern's school?",
         name: "school",
-        validate: answer => {
-            if (answer !== "") {
-                return true;
-            } else
-            return "Please enter your school's name.";
-        }
+        // validate: answer => {
+        //     if (answer !== "") {
+        //         return true;
+        //     } else
+        //     return "Please enter your school's name.";
+        // }
     },
-    ]))
-    .then(inquirer.prompt([
     {
         type: "list",
         message: "Which type of team member would you like to add?",
         name: "newMember",
-        choices: ["Engineer", "Intern", "I don't need any more team members."]
-    }
-    ]))
-}
+        choices: ["Engineer", "Intern", "I don't need anymore team members."],
+    },
+    ]).then(val => {
+        if (val.newMember === "Engineer") {
+            return engineerTeam();
+        } else if (val.newMember === "Intern") {
+            return internTeam();
+        } else  
+            return quit();
+        
+    })
+    );
+}; 
+  
 
-function validateNumber(number) {
-    var isValid = !_.isNaN(parseFloat(number));
-    return isValid || "It should be a number.";
-}
 
-function validateEmail(email) {
-    return Joi.validate(email, Joi.string().email(), onValidation);
+createTeam()
+    
+
+    
+
+
+
+
+
+// function newMember() {
+//     return inquirer.prompt([
+//         {
+//             type: "list",
+//             message: "Which type of team member would you like to add?",
+//             name: "newMember",
+//             choices: ["Engineer", "Intern", "I don't need anymore team members."],
+//             validate: answer => {
+//                 if (answer == "engineer") {
+//                     return engineerTeam;
+//                 } else if (answer == "intern") {
+//                     return internTeam;
+//                 } else {
+//                     return ("I don't need anymore team members.")
+//                 }
+//             }
+//         }
+//     ]);
+// };
+
+function quit(){
+    console.log("Team is complete!");
+    
 }
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-render(manager, engineer, intern);
+
+// let array = [manager, engineer, intern];
+// render.apply(null, array)
+//     fs.writeFileSync("team.html", array, function(err) {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log("Successfully wrote to team.html")
+//     });
+
+
+
+
+// render(manager, engineer, intern)
+//     .then(function(answers) {
+//         fs.writeFileAsync("team.html", answers, function(err){
+
+//             if (err) {
+//                 return console.log(err);
+//             }
+
+//             console.log("Successfully wrote to team.html")
+//         });
+//     })
+//     .then(function() {
+       
+//     })
+
+
+
         
    
 // After you have your html, you're now ready to create an HTML file using the HTML
